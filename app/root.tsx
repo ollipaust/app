32b6fb6b20a04ab633f8df2482e2776a5dc11d58
Olli Paust <ollipaust@gmail.com>
2024-01-25 09:46:44 +0100

    isolate headers function

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,13 @@
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
-import type { HeadersFunction, LinksFunction } from "@remix-run/node";
-import parseCacheControl from "parse-cache-control";
-
+import type { LinksFunction } from "@remix-run/node";
+import { responseHeaders } from "~/utils/responseHeaders";
 import Layout from "~/ui/template/layout";
-
 import baseStylesheet from "~/ui/styles/base.css";
 
+// HTTP Headers
+export const headers = responseHeaders;
+
+// Styles
 export const links: LinksFunction = () => {
 	return [
 		{
@@ -23,34 +25,7 @@ export const links: LinksFunction = () => {
 	];
 };
 
-export const headers: HeadersFunction = ({ loaderHeaders, parentHeaders }) => {
-    const defaultMaxAge = 31560000;
-
-    const getCacheMaxAge = (headers: Headers) => {
-        const cacheControl = headers.get("Cache-Control");
-        const parsed = cacheControl ? parseCacheControl(cacheControl) : null;
-        return parsed && typeof parsed["max-age"] === "number" ? parsed["max-age"] : defaultMaxAge;
-    };
-
-    const loaderMaxAge = getCacheMaxAge(loaderHeaders);
-    const parentMaxAge = getCacheMaxAge(parentHeaders);
-
-    const maxAge = Math.min(loaderMaxAge, parentMaxAge);
-
-    return {
-        "Cache-Control": `max-age=${maxAge}, s-maxage=${maxAge * 12}`,
-        "Content-Security-Policy": "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'self'; base-uri 'self';",
-        "X-Frame-Options": "SAMEORIGIN",
-        "Referrer-Policy": "strict-origin-when-cross-origin",
-        "Permissions-Policy": "geolocation=(self), microphone=()",
-        "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
-        "X-Content-Type-Options": "nosniff",
-		"Cross-Origin-Embedder-Policy": "require-corp",
-        "Cross-Origin-Opener-Policy": "same-origin",
-        "Cross-Origin-Resource-Policy": "same-site",
-    };
-};
-
+// Application
 export default function App() {
 	return (
 		<html lang="en">
---
+++ app/utils/responseHeaders.tsx
@@ -0,0 +1,30 @@
+import type { HeadersFunction } from "@remix-run/node";
+import parseCacheControl from "parse-cache-control";
+
+export const responseHeaders: HeadersFunction = ({ loaderHeaders, parentHeaders }) => {
+    const defaultMaxAge = 31560000;
+
+    const getCacheMaxAge = (headers: Headers) => {
+        const cacheControl = headers.get("Cache-Control");
+        const parsed = cacheControl ? parseCacheControl(cacheControl) : null;
+        return parsed && typeof parsed["max-age"] === "number" ? parsed["max-age"] : defaultMaxAge;
+    };
+
+    const loaderMaxAge = getCacheMaxAge(loaderHeaders);
+    const parentMaxAge = getCacheMaxAge(parentHeaders);
+
+    const maxAge = Math.min(loaderMaxAge, parentMaxAge);
+
+    return {
+        "Cache-Control": `max-age=${maxAge}, s-maxage=${maxAge * 12}`,
+        "Content-Security-Policy": "default-src 'self'; script-src 'self'; style-src 'self' https://cdnjs.cloudflare.com; object-src 'none'; frame-ancestors 'self'; base-uri 'self';",
+        "X-Frame-Options": "SAMEORIGIN",
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+        "Permissions-Policy": "geolocation=(self), microphone=()",
+        "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
+        "X-Content-Type-Options": "nosniff",
+		"Cross-Origin-Embedder-Policy": "require-corp",
+        "Cross-Origin-Opener-Policy": "same-origin",
+        "Cross-Origin-Resource-Policy": "same-site",
+    };
+};
