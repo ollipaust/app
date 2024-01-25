a07b9b18d6b79f6c68f580dbeb2cf9ee9ffd21e6
Olli Paust <ollipaust@gmail.com>
2024-01-25 11:51:24 +0100

    add sitemap and robots generator

---
+++ app/routes/robots[.]txt.tsx
@@ -0,0 +1,8 @@
+import { generateRobotsTxt } from '@nasa-gcn/remix-seo'
+import siteConfig from '~/siteConfig'
+
+export function loader() {
+	return generateRobotsTxt([
+		{ type: 'sitemap', value: `${ siteConfig.siteUrl }/sitemap.xml` },
+	])
+}
